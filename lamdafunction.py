8217c0b414885add579bff0c18e7b1b06ec372d7
calebmcook <caleb.cook@gmail.com>
2022-11-17 03:46:16 +0000

    updates

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_Name='pytorch-inference-2022-11-13-04-23-45-837-REDEPLOY'
 
 def lambda_handler(event, context):
 
